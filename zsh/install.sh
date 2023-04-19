2cc4a4f6217e6b01144bef24f6add53de382e477
Egor Chunaev <ch.egor.yu@gmail.com>
2023-04-19 15:04:28 +0200

    add install.sh for zsh, better install.sh for tmux

---
+++ zsh/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Oh My Zsh.
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# PowerLevel10K.
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+# Zsh plugins.
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting

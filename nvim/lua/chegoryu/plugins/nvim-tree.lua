a54be7822336b8cce09abb1e53575f40a8e41df6
Egor Chunaev <ch.egor.yu@gmail.com>
2023-04-19 23:11:16 +0200

    better whitespace control; retab config

--- nvim/lua/chegoryu/plugins/nvim-tree.lua
+++ nvim/lua/chegoryu/plugins/nvim-tree.lua
@@ -1,6 +1,6 @@
 local setup, nvimtree = pcall(require, "nvim-tree")
 if not setup then
-	return
+    return
 end
 
 -- Recommended settings from nvim-tree documentation.
@@ -12,47 +12,47 @@ vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 
 -- Configure nvim-tree.
 nvimtree.setup({
-	-- Change folder arrow icons.
-	renderer = {
-		icons = {
-			glyphs = {
-				folder = {
-					arrow_closed = "",
-					arrow_open = "",
-				},
-			},
-		},
-	},
-	-- Disable window_picker for explorer to work well with window splits.
-	actions = {
-		open_file = {
-			window_picker = {
-				enable = false,
-			},
-		},
-	},
+    -- Change folder arrow icons.
+    renderer = {
+        icons = {
+            glyphs = {
+                folder = {
+                    arrow_closed = "",
+                    arrow_open = "",
+                },
+            },
+        },
+    },
+    -- Disable window_picker for explorer to work well with window splits.
+    actions = {
+        open_file = {
+            window_picker = {
+                enable = false,
+            },
+        },
+    },
 })
 
 -- Open nvim-tree on startup.
 
 local function open_nvim_tree(data)
-	-- Buffer is a [No Name].
-	local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+    -- Buffer is a [No Name].
+    local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
 
-	-- Buffer is a directory.
-	local directory = vim.fn.isdirectory(data.file) == 1
+    -- Buffer is a directory.
+    local directory = vim.fn.isdirectory(data.file) == 1
 
-	if not no_name and not directory then
-		return
-	end
+    if not no_name and not directory then
+        return
+    end
 
-	-- Change to the directory.
-	if directory then
-		vim.cmd.cd(data.file)
-	end
+    -- Change to the directory.
+    if directory then
+        vim.cmd.cd(data.file)
+    end
 
-	-- Open the tree.
-	require("nvim-tree.api").tree.open()
+    -- Open the tree.
+    require("nvim-tree.api").tree.open()
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })

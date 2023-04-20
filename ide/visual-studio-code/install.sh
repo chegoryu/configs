9bea710371eed6ffaf83af2357d03d6000080d92
Egor Chunaev <ch.egor.yu@gmail.com>
2023-04-20 13:14:23 +0200

    export vscode extensions

---
+++ ide/visual-studio-code/install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+code --install-extension CS128.cs128-clang-tidy
+code --install-extension golang.go
+code --install-extension llvm-vs-code-extensions.vscode-clangd
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-toolsai.vscode-jupyter-cell-tags
+code --install-extension ms-toolsai.vscode-jupyter-slideshow
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension ms-vscode.cpptools-themes
+code --install-extension ms-vscode.remote-explorer
+code --install-extension rust-lang.rust-analyzer
+code --install-extension SanaAjani.taskrunnercode
+code --install-extension twxs.cmake

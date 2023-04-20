e3c7cbad8500ce6fb44bd2a16513deb39833bb64
Egor Chunaev <ch.egor.yu@gmail.com>
2023-04-20 18:22:17 +0200

    add go integration

---
+++ templates/go/competitive_programming/cp_main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+var (
+    in  = bufio.NewReader(os.Stdin)
+    out = bufio.NewWriter(os.Stdout)
+)
+
+func main() {
+    defer out.Flush()
+    solve()
+}
+
+const (
+    INF = 1000 * 1000 * 1000 + 21
+    LLINF = (1 << 60) + 5
+    MOD = 1000 * 1000 * 1000 + 7
+)
+
+func solve() {
+    {{_cursor_}}
+}

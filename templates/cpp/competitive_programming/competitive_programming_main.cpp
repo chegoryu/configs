e6f5e86c4ba9a102858faef98256f6b116886896
Egor Chunaev <ch.egor.yu@gmail.com>
2023-04-20 14:52:59 +0200

    add global clangd config; better competitive_programming_main template

--- templates/cpp/competitive_programming/competitive_programming_main.cpp
+++ templates/cpp/competitive_programming/competitive_programming_main.cpp
@@ -1,41 +1,63 @@
 #include <stdio.h>
-#include <iostream>
 #include <stdlib.h>
-#include <string>
-#include <string.h>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <set>
-#include <map>
-#include <assert.h>
-#include <algorithm>
-#include <iomanip>
-#include <time.h>
-#include <math.h>
-#include <bitset>
 
-#pragma comment(linker, "/STACK:256000000")
+#include <assert.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+
+#include <iostream>
+#include <iomanip>
+
+#include <algorithm>
+#include <chrono>
+#include <complex>
+#include <random>
+
+#include <array>
+#include <bitset>
+#include <deque>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+// #pragma comment(linker, "/STACK:256000000")
 
 using namespace std;
 
-typedef long long int ll;
-typedef long double ld;
+using ll = long long int;
+using ull = unsigned long long int;
+using ld = long double;
 
-const int INF = 1000 * 1000 * 1000 + 21;
-const ll LLINF = (1ll << 60) + 5;
-const int MOD = 1000 * 1000 * 1000 + 7;
+#ifdef CHEGORYU
+mt19937_64 rng(227);
+#else
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+#endif
+
+ll my_rand(ll n) {
+	return (ull)rng() % n;
+}
+
+[[maybe_unused]] const int INF = 1000 * 1000 * 1000 + 21;
+[[maybe_unused]] const ll LLINF = (1ll << 60) + 5;
+[[maybe_unused]] const int MOD = 1000 * 1000 * 1000 + 7;
 
 
 int main() {
-#ifdef CH_EGOR
+#ifdef CHEGORYU
     freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
 #else
     // freopen("", "r", stdin);
     // freopen("", "w", stdout);
 #endif
+
 
     {{_cursor_}}
 

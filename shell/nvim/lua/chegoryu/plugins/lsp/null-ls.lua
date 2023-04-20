aee382e38d3acb421de54d99b24aac4397bb8976
Egor Chunaev <ch.egor.yu@gmail.com>
2023-04-21 00:17:41 +0200

    remove unused variable

--- shell/nvim/lua/chegoryu/plugins/lsp/null-ls.lua
+++ shell/nvim/lua/chegoryu/plugins/lsp/null-ls.lua
@@ -4,7 +4,6 @@ if not setup then
 end
 
 local formatting = null_ls.builtins.formatting
-local diagnostics = null_ls.builtins.diagnostics
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 

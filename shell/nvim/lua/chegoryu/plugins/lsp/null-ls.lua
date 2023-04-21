4f213e8268b10d83967e640805b3303216a9b7c3
Egor Chunaev <ch.egor.yu@gmail.com>
2023-04-22 00:38:39 +0200

    add keymap for manual code formatting

--- shell/nvim/lua/chegoryu/plugins/lsp/null-ls.lua
+++ shell/nvim/lua/chegoryu/plugins/lsp/null-ls.lua
@@ -7,10 +7,11 @@ local formatting = null_ls.builtins.formatting
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-local bad_filetypes = {
-    "go",
-}
 local function is_autoformatting_disabled(filetype)
+    local bad_filetypes = {
+        "go",
+    }
+
     for _, bad_filetype in pairs(bad_filetypes) do
         if filetype == bad_filetype then
             return true
@@ -29,23 +30,30 @@ null_ls.setup({
 
     -- Configure format on save.
     on_attach = function(current_client, bufnr)
-        local filetype = vim.fn.getbufvar(bufnr, "&filetype")
+        if current_client.supports_method("textDocument/formatting") then
+            local function format_code()
+                vim.lsp.buf.format({
+                    filter = function(client)
+                        -- Only use null-ls for formatting instead of lsp server.
+                        return client.name == "null-ls"
+                    end,
+                    bufnr = bufnr,
+                })
+            end
 
-        if not is_autoformatting_disabled(filetype) and current_client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({
-                        filter = function(client)
-                            -- Only use null-ls for formatting instead of lsp server.
-                            return client.name == "null-ls"
-                        end,
-                        bufnr = bufnr,
-                    })
-                end,
-            })
+            vim.keymap.set("n", "<leader>cf", format_code)
+
+            if not is_autoformatting_disabled(vim.fn.getbufvar(bufnr, "&filetype")) then
+                vim.api.nvim_clear_autocmds({
+                    group = augroup,
+                    buffer = bufnr,
+                })
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = augroup,
+                    buffer = bufnr,
+                    callback = format_code,
+                })
+            end
         end
     end,
 })

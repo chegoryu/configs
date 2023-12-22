f346bd20a6248b22fd1cf97ab668f6fd692e2ef8
Egor Chunaev <egor.chunaev@pinely.com>
2023-12-22 19:47:55 +0100

    fix dev icons setup

--- shell/nvim/lua/chegoryu/plugins/nvim-web-devicons.lua
+++ shell/nvim/lua/chegoryu/plugins/nvim-web-devicons.lua
@@ -1,19 +1,21 @@
 return {
     -- Vs-code like icons.
     "nvim-tree/nvim-web-devicons",
-    opts = {
-        -- Fix some weird icons.
-        toml = {
-            icon = "",
-            color = "#6d8086",
-            cterm_color = "66",
-            name = "Toml",
-        },
-        gql = {
-            icon = "",
-            color = "#e535ab",
-            cterm_color = "199",
-            name = "GraphQL",
-        },
-    },
+    config = function()
+        require("nvim-web-devicons").set_icon({
+            -- Fix some weird icons.
+            toml = {
+                icon = "",
+                color = "#6d8086",
+                cterm_color = "66",
+                name = "Toml",
+            },
+            gql = {
+                icon = "",
+                color = "#e535ab",
+                cterm_color = "199",
+                name = "GraphQL",
+            },
+        })
+    end,
 }

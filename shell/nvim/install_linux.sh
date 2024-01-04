a3a5609d53c1471f5d67407e41ca6251e8e984f4
Egor Chunaev <egor.chunaev@pinely.com>
2024-01-04 12:29:29 +0100

    fix shebang; update brew list

--- shell/nvim/install_linux.sh
+++ shell/nvim/install_linux.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -e
+#!/usr/bin/env -S bash -e
 
 # Be careful, the build will succeed even if some dependencies are missing,
 # but some neovim features will not work (for example tmux clipboard integration).
--- shell/zsh/install_dependencies.sh
+++ shell/zsh/install_dependencies.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -e
+#!/usr/bin/env -S bash -e
 
 source $(dirname $0)/../common/git.sh
 

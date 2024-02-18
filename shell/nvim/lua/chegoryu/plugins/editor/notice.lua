0a3edc395bc8a70f33e3b58cc3befed417811066
Egor Chunaev <ch.egor.yu@gmail.com>
2024-02-18 23:51:42 +0100

    add notice plugin

---
+++ shell/nvim/lua/chegoryu/plugins/editor/notice.lua
@@ -0,0 +1,19 @@
+return {
+    -- Better UI.
+    "folke/noice.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+    },
+    -- event = { "VeryLazy" },
+    opts = {
+        lsp = {
+            -- Override markdown rendering so that cmp and other plugins use Treesitter.
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
+            },
+        },
+    },
+}

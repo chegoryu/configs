4e3da61533e9796b88074c3b0c41f11825af168b
Egor Chunaev <egor.chunaev@pinely.com>
2024-07-27 17:44:48 +0200

    allow to configure clangd and clang format

--- shell/nvim/lua/chegoryu/core/config.lua
+++ shell/nvim/lua/chegoryu/core/config.lua
@@ -1,8 +1,20 @@
 local is_pinely = os.getenv("NVIM_PINELY") ~= nil
-local python_path = os.getenv("NVIM_PYTHON_PATH")
 
+local python_path = os.getenv("NVIM_PYTHON_PATH")
 if python_path == nil and is_pinely then
     python_path = "/usr/bin/twix-python"
+end
+
+local clang_format_path = os.getenv("NVIM_CLANG_FORMAT_PATH")
+if clang_format_path == nil and is_pinely then
+    clang_format_path = "clang-format-18"
+end
+
+local clangd_path = os.getenv("NVIM_CLANGD_PATH")
+if clangd_path == nil and is_pinely then
+    clangd_path = "clangd-18"
+elseif clangd_path == nil then
+    clangd_path = "clangd"
 end
 
 return {
@@ -20,4 +32,8 @@ return {
     USE_BLACK_FROM_PYTHON = os.getenv("NVIM_USE_BLACK_FROM_PYTHON") ~= nil,
     PYTHON_VENV_PATH = os.getenv("NVIM_PYTHON_VENV_PATH"),
     PYTHON_PATH = python_path,
+
+    -- C++ options.
+    CLANG_FORMAT_PATH = clang_format_path,
+    CLANGD_PATH = clangd_path,
 }

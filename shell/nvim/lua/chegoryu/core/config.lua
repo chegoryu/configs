aa31c32d63dbcd198b1ee8933d9a2837b15607d5
Egor Chunaev <ch.egor.yu@gmail.com>
2024-09-04 14:46:00 +0200

    resolve other repo hases in repolink

--- shell/nvim/lua/chegoryu/core/config.lua
+++ shell/nvim/lua/chegoryu/core/config.lua
@@ -1,5 +1,14 @@
 local is_pinely = os.getenv("NVIM_PINELY") ~= nil
 
+local file_exists = function(path)
+    local file = io.open(path, "r")
+    if file == nil then
+        return false
+    end
+    io.close(file)
+    return true
+end
+
 local python_path = os.getenv("NVIM_PYTHON_PATH")
 if python_path == nil and is_pinely then
     python_path = "/usr/bin/twix-python"
@@ -17,10 +26,35 @@ elseif clangd_path == nil then
     clangd_path = "clangd"
 end
 
+local pinely_private_config = {}
+local pinely_private_config_file_path = os.getenv("NVIM_PINELY_PRIVATE_CONFIG_FILE_PATH")
+local default_pinely_private_config_file_path = os.getenv("HOME") .. "/.config/pinely/nvim/private_config.json"
+if pinely_private_config_file_path == nil and is_pinely and file_exists(default_pinely_private_config_file_path) then
+    pinely_private_config_file_path = default_pinely_private_config_file_path
+end
+
+if pinely_private_config_file_path ~= nil then
+    local ok, result = pcall(vim.fn.readblob, pinely_private_config_file_path)
+    if not ok then
+        error("failed to read pinely private config file: " .. pinely_private_config_file_path)
+    else
+        ok, pinely_private_config = pcall(vim.fn.json_decode, result)
+        if not ok then
+            error("failed to parse pinely private config file: " .. pinely_private_config_file_path)
+        end
+    end
+end
+
+local pinely_repo_paths = pinely_private_config["repo_paths"]
+if pinely_repo_paths == nil then
+    pinely_repo_paths = {}
+end
+
 return {
     -- Enable pinely specific things (codestyle/special python version/etc).
     IS_PINELY = is_pinely,
     PINELY_GIT_HOST = os.getenv("NVIM_PINELY_GIT_HOST"),
+    PINELY_REPO_PATHS = pinely_repo_paths,
 
     -- Formatting.
     FORMAT_ON_SAVE_ALL = os.getenv("NVIM_FORMAT_ON_SAVE_ALL") ~= nil,

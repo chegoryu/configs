1261bdf5152facf441b26a9c54b5bb1a810418d0
Egor Chunaev <ch.egor.yu@gmail.com>
2025-06-17 01:16:12 +0200

    try our best to pass global capabilities everywhere

--- shell/nvim/lua/chegoryu/core/setup.lua
+++ shell/nvim/lua/chegoryu/core/setup.lua
@@ -5,6 +5,3 @@
 -- Disable deprecation warnings like "Defining diagnostic signs with :sign-define or sign_define() is deprecated. Run ":checkhealth vim.deprecated" for more information".
 ---@diagnostic disable-next-line: duplicate-set-field
 vim.deprecate = function() end
-
--- Enable inline diagnostics.
-vim.diagnostic.config({ virtual_text = true })
